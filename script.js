a7cf171f4b6454c8a23154b85014d61c87635f6e
Aurob <robau6@gmail.com>
2018-11-15 10:44:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function left(){
     if(day <10){
         day = "0"+day;
     }
-    var url = 'https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-01-'+day+'&cloud_score=True&api_key=DEMO_KEYVJA4indt7zBEIj6P4wLp0UngsMQmhzqvDtMnh6f9'
+    var url = 'https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-01-'+day+'&cloud_score=True&api_key=VJA4indt7zBEIj6P4wLp0UngsMQmhzqvDtMnh6f9'
     console.log("left\n"+day+"\n"+url);
     document.getElementById('info').innerHTML= new Date();
     fetch(url)
@@ -25,13 +25,15 @@ function right(){
     if(day <10){
         day = "0"+day;
     }
-    console.log('left '+day);
+    var url = 'https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-01-'+day+'&cloud_score=True&api_key=VJA4indt7zBEIj6P4wLp0UngsMQmhzqvDtMnh6f9'
+    console.log("left\n"+day+"\n"+url);
     document.getElementById('info').innerHTML= new Date();
-    fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-01-'+day+'&cloud_score=True&api_key=VJA4indt7zBEIj6P4wLp0UngsMQmhzqvDtMnh6f9')
+    fetch(url)
             .then(function(response) {
             return response.json();
         }).then(function(myJson) {
         url = JSON.stringify(myJson.url).replace('"','').replace('"','');
+        console.log(url);
         document.getElementById('img').src = url;
     });
     if(day >29){
