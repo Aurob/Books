851eebf9d3fa01a0af03190fb2779060f9191467
Aurob <robau6@gmail.com>
2018-11-15 10:13:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ function left(day){
         .then(function(response) {
         return response.json();
     }).then(function(myJson) {
-    console.log(JSON.stringify(myJson));
+    url = JSON.stringify(myJson).url;
+    document.getElementById('img').src = url;
   });
 }
 function right(day){
     console.log('right');
-  const userAction = async () => {
-    const response = await fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY');
-    const myJson = await response.json(); //extract JSON from the http response
-    console.log(myJson);
-    // do something with myJson
-  }
-  document.getElementById('info').innerHTML= new Date();
+    document.getElementById('info').innerHTML= new Date();
+    fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY')
+            .then(function(response) {
+            return response.json();
+    }).then(function(myJson) {
+    url = JSON.stringify(myJson).url;
+    document.getElementById('img').src = url;
+  });
 }
