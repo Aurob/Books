6a7d16a1acad9332a51a1a730d51f6b77804f88e
Aurob <robau6@gmail.com>
2018-11-15 10:11:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function left(day){
+    console.log('left');
     document.getElementById('info').innerHTML= new Date();
     fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY')
         .then(function(response) {
@@ -8,6 +9,7 @@ function left(day){
   });
 }
 function right(day){
+    console.log('right');
   const userAction = async () => {
     const response = await fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY');
     const myJson = await response.json(); //extract JSON from the http response
