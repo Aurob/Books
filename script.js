634549e0341462c2888296f0f439513671fd7131
Aurob <robau6@gmail.com>
2018-11-15 10:07:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function query(){
+function left(day){
+    const userAction = async () => {
+      const response = await fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY');
+      const myJson = await response.json(); //extract JSON from the http response
+      console.log(myJson);
+      // do something with myJson
+    }
+  document.getElementById('info').innerHTML= new Date();
+}
+function right(day){
+  const userAction = async () => {
+    const response = await fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY');
+    const myJson = await response.json(); //extract JSON from the http response
+    console.log(myJson);
+    // do something with myJson
+  }
   document.getElementById('info').innerHTML= new Date();
 }
