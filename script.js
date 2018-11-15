b4b4695d7f7e5c3d13ba09628e791c207bfb64aa
Aurob <robau6@gmail.com>
2018-11-15 10:33:33 -0600

    fix

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 function left(){
     day = document.getElementById('day').innerHTML;
     if(day <10){
-        day = "0"+day
+        day = "0"+day;
     }
     console.log('left '+day);
     document.getElementById('info').innerHTML= new Date();
-    fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+(day-1)+'-01&cloud_score=True&api_key=DEMO_KEY')
+    fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY')
             .then(function(response) {
             return response.json();
         }).then(function(myJson) {
         url = JSON.stringify(myJson.url).replace('"','').replace('"','');
         document.getElementById('img').src = url;
     });
-    day = day-1;
-    if(day < 0){
-        day = 0;
+    if(day < 2){
+        day = 2;
     }
+    document.getElementById('day').innerHTML = day-1;
+    
 }
 function right(){
     day = document.getElementById('day').innerHTML
     if(day <10){
-        day = "0"+day
+        day = "0"+day;
     }
     console.log('left '+day);
     document.getElementById('info').innerHTML= new Date();
-    fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+(day+1)+'-01&cloud_score=True&api_key=DEMO_KEY')
+    fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY')
             .then(function(response) {
             return response.json();
         }).then(function(myJson) {
         url = JSON.stringify(myJson.url).replace('"','').replace('"','');
         document.getElementById('img').src = url;
     });
-    day = day+1;
-    if(day < 0){
-        day = 0;
+    if(day >29){
+        day = 29;
     }
+    document.getElementById('day').innerHTML = day+1;
+    
 }
