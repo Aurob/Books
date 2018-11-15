869034d7688b460253e0a0f10fea2ab48fb26907
Aurob <robau6@gmail.com>
2018-11-15 10:21:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function left(day){
     console.log('left');
     document.getElementById('info').innerHTML= new Date();
     fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY')
-        .then(function(response) {
-        return response.json();
-    }).then(function(myJson) {
-    url = JSON.stringify(myJson.url);
-    document.getElementById('img').src = url;
-  });
+            .then(function(response) {
+            return response.json();
+        }).then(function(myJson) {
+        url = JSON.stringify(myJson.url).replace('"','');
+        document.getElementById('img').src = url;
+    });
 }
 function right(day){
     console.log('right');
@@ -15,8 +15,8 @@ function right(day){
     fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+day+'-01&cloud_score=True&api_key=DEMO_KEY')
             .then(function(response) {
             return response.json();
-    }).then(function(myJson) {
-    url = JSON.stringify(myJson).url;
-    document.getElementById('img').src = url;
-  });
+        }).then(function(myJson) {
+        url = JSON.stringify(myJson.url).replace('"','');
+        document.getElementById('img').src = url;
+    });
 }
