7aa65a919650ab8fe24e05e4a35b4c3780ea1f28
Aurob <robau6@gmail.com>
2018-11-15 10:30:13 -0600

    fix

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function left(){
-    day = document.getElementById('day').value
-    console.log('left');
+    day = document.getElementById('day').innerHTML;
+    if(day <10){
+        day = "0"+day
+    }
+    console.log('left '+day);
     document.getElementById('info').innerHTML= new Date();
     fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+(day-1)+'-01&cloud_score=True&api_key=DEMO_KEY')
             .then(function(response) {
@@ -10,10 +13,16 @@ function left(){
         document.getElementById('img').src = url;
     });
     day = day-1;
+    if(day < 0){
+        day = 0;
+    }
 }
 function right(){
-    day = document.getElementById('day').value
-    console.log('right');
+    day = document.getElementById('day').innerHTML
+    if(day <10){
+        day = "0"+day
+    }
+    console.log('left '+day);
     document.getElementById('info').innerHTML= new Date();
     fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+(day+1)+'-01&cloud_score=True&api_key=DEMO_KEY')
             .then(function(response) {
@@ -23,4 +32,7 @@ function right(){
         document.getElementById('img').src = url;
     });
     day = day+1;
+    if(day < 0){
+        day = 0;
+    }
 }
