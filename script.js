218187bf8e97d5725ef076fde2377a1273b886eb
Rob Au <robau6@gmail.com>
2019-07-04 01:00:15 -0500

    .

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var month = 1
 function left(){
-    if(month>12) month = 1;
+    if(month<1) month = 12;
     var day = parseInt(document.getElementById('day').innerHTML);
     if(day <10){
         day = "0"+day;
     }
+    if(month <10){
+        month = "0"+day;
+    }
     var url = 'https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2017-'+month+'-'+day+'&cloud_score=True&api_key=VJA4indt7zBEIj6P4wLp0UngsMQmhzqvDtMnh6f9'
-    console.log("left\n"+day+"\n"+url);
-    document.getElementById('info').innerHTML= new Date();
+    
+    document.getElementById('info').innerHTML= '2017-'+month+'-'+day;
     fetch(url)
             .then(function(response) {
             return response.json();
@@ -16,11 +19,11 @@ function left(){
         console.log(url);
         document.getElementById('img').src = url;
     });
-    if(day >=30){
-        day = 2;
-	month++;
+    if(day <1){
+        day = 30;
+	    month--;
     }
-    document.getElementById('day').innerHTML = day++;
+    document.getElementById('day').innerHTML = day--;
     
 }
 function right(){
